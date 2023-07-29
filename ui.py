22c8ee4f4feee5626659dda31c25253fc903487a
Sandra <sandrallancherosg@gmail.com>
2023-07-29 16:44:38 +0200

    added notebooks: original and helper. Renamed files to improve code

--- ui.py
+++ ui.py
@@ -1,5 +1,5 @@
 import gradio as gr
-import pipeline
+import video_tools
 title = "Add Text/Caption to your YouTube Shorts - MultiLingual"
 
 block = gr.Blocks()
@@ -16,8 +16,8 @@ with block:
                     mirror_webcam = False
                 )
                 op_video = gr.Video()
-        btn = gr.Button("Generate Subtitle Video")
-        btn.click(pipeline.create_output_video, inputs=[inp_video], outputs=[op_video])
+        btn = gr.Button("Generate Meeting Summary")
+        btn.click(video_tools.create_output_video, inputs=[inp_video], outputs=[op_video])
 
         gr.HTML('''
         <div class="footer">
