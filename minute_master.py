cda90ac34d2198dc3fd54154ba4a91f01d2a6f54
Sandra <sandrallancherosg@gmail.com>
2023-07-30 02:04:06 +0200

    added Processor and MinuteMaster classes

---
+++ minute_master.py
@@ -0,0 +1,51 @@
+from transformers import pipeline
+import processor
+import video_tools
+
+class MinuteMaster(processor.Processor):
+    def __init__(self):
+        self.filters = list()
+        self.meeting_summary= {'input_video': object(),
+           'audio_file': object(),
+           'transcript': object(),
+           'output_video':object(),
+           'content': str(),
+           'summary':str(),
+           'action_items':[]
+           }
+        self.meet_summarizer= pipeline("summarization")
+ 
+    def assign_input_video( self,input_video):
+        self.meeting_summary['input_video']= input_video
+
+    def get_audio(self):
+        self.meeting_summary['audio_file'] = video_tools.video2mp3(self.meeting_summary['input_video'], output_ext="mp3" )
+
+    def get_transcript(self):
+        self.meeting_summary['transcript'] = video_tools.translated_transcript( self.meeting_summary['audio_file'] )
+
+    def generate_subtitled_video(self):
+        self.meeting_summary['audio_file'] = video_tools.create_output_video(self.meeting_summary['audio_file'], self.meeting_summary['input_video'],meeting_summary['transcript'])
+
+    def get_meeting_content(self):
+        self.meeting_summary['content'] = self.meeting_summary['transcript']['text']
+
+    def summarize(self):
+        self.meeting_summary['summary'] = self.summarizer(self.meeting_summary['content'] )
+
+    def process(self):
+        self.add([self.assign_input_video,self.get_audio,self.get_transcript, self.generate_subtitled_video,self.get_meeting_content,self.summarize])
+        self.execute(self.meeting_summary)
+        print(self.meeting_summary['summary'])
+        return self.meeting_summary
+
+    def get_meeting_summary(self):
+        return self.meeting_summary
+    
+    def set_input_video(self,input_video):
+        self.
+
+
+
+ 
+
--- processor.py
+++ processor.py
@@ -1,5 +1,3 @@
-
-
 class Processor():
     def __init__(self):
         self.filters = list()
@@ -10,5 +8,25 @@ class Processor():
     def execute(self, task):
         print("Executing pipeline...")
         for task_filter in self.filters:
+            print("Executing step "+ task_filter.__name__ + " ...")
             task_filter(task)
+            print(" Done!")
  
+def double_data(task):
+    task['data'] = [x*2 for x in task['data']]
+ 
+def update_header(task):
+    task['header'] = 'this is the updated message'
+ 
+simple_processor = Processor()
+ 
+simple_processor.add([double_data,
+              update_header]) # note we are adding the function object, not making a function call
+ 
+task = {'header': 'this is the original message',
+           'data': [1,2,3]}
+ 
+simple_processor.execute(task)
+
+for key, value in task.items():
+    print(key + ' : ' + str(value))
